9c6d0f3160b80f3b517c9921c548d1c7e635ccbb
Ahsan-bashir <bteeccc@gmail.com>
2023-09-19 22:34:32 +0500

    fixed some issues

--- Week 2/Mongo-Integration/app.js
+++ Week 2/Mongo-Integration/app.js
@@ -1,13 +1,14 @@
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb")
 
-const connectionURI = "mongodb://localhost:27017"
+const connectionURI = "mongodb://127.0.0.1:27017"
 
 const client = new MongoClient(connectionURI)
 client.connect().then(() => {
+
     console.log("Connected Successfully");
 })
-.catch(() => {
-        console.log("Connection Failed !!!");
+.catch((err) => {
+        console.log("Connection Failed !!!",err);
 
     })
 
